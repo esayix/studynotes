75aa3e4c1ff46ee29ae729618559392ad4cde850
C117 <c117ogrenci@gmail.com>
2023-10-06 09:34:03 +0300

    Proje Açıldı

---
+++ history.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Fri Oct  6 08:37:25 2023)---
+runfile('C:/Users/C-117/.spyder-py3/temp.py', wdir='C:/Users/C-117/.spyder-py3')
+runfile('C:/Users/C-117/.spyder-py3/study1py.py', wdir='C:/Users/C-117/.spyder-py3')
+type(sehir)
---
+++ study1py.py
@@ -0,0 +1,29 @@
+print("mERHABA");
+
+#sdsad
+
+"""
+ghj
+"""
+
+
+a = 13123;
+b = 21;
+
+sonuc = a+b ;
+print(sonuc);
+
+ay = 12;
+kilometre = 3000.0;
+sehir = "Isparta";
+
+print(ay);
+print(kilometre);
+print(sehir);
+
+a = 5;
+ay = "Ekim";
+
+print(len(ay));
+
+print(sehir,kilometre)
